2fc4f9b85f747660053fcf79aa0f74adf397db52
Joshua Eichorn <joshua.eichorn@pagely.com>
2014-03-18 16:30:36 +0000

    Template support

--- src/GraphiteAlert/MonitorCommand.php
+++ src/GraphiteAlert/MonitorCommand.php
@@ -42,8 +42,39 @@ class MonitorCommand extends Command
 
         $options['graphite_url'] = $config['graphite']['url'];
 
+        $metrics = $this->processTemplates($config['metrics']);
+
         $graphite = new GraphiteData($config['graphite']);
-        $monitor = new GraphiteMonitor($output, $alterers, $graphite, $config['metrics'], $options);
+        $monitor = new GraphiteMonitor($output, $alterers, $graphite, $metrics, $options);
+
+
+        $output->writeln("<info>Found ".count($metrics)." metrics</info>");
+        foreach(array_keys($metrics) as $metric)
+            $output->writeln("<info>    $metric</info>");
+
         $monitor->monitor();
+    }
+
+    //@todo refactor this
+    protected function processTemplates($metrics)
+    {
+        foreach($metrics as $metric => $config)
+        {
+            if (isset($config['template']))
+            {
+                unset($metrics[$metric]);
+                foreach($config['keys'] as $key)
+                {
+                    foreach($config['template'] as $m => $c)
+                    {
+                        $m = sprintf($m, $key);
+                        $c['target'] = sprintf($c['target'], $key);
+                        $metrics[$m] = $c;
+                    }
+                }
+            }
+        }
+
+        return $metrics;
     }
 }
